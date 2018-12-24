21493b82336f49880792828348b6f7e60f85c78f
SoarUI <ecsuperpay@163.com>
2018-12-24 15:56:40 +0800

    修正错误

--- select.cpp
+++ select.cpp
@@ -2,12 +2,25 @@
 #include<sys/select.h>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<errno.h>
 #include<netinet/in.h>
 #include<sys/time.h>
 #include<stdio.h>
 #include<stdlib.h>
 #define MAXCLIENT 100
-
+#include<string.h>
+#include<fcntl.h>
+void convnonblock(int fd){
+int flags =0;
+if((flags= fcntl(fd,F_GETFL))<0){
+perror("fcntl error!\n");
+return;}
+flags |=O_NONBLOCK;
+if(fcntl(fd,F_SETFL,flags) ){
+perror("fcntl(F_SETFL)\n");
+exit(1);
+}
+}
 int main()
 {
 int serverfd,client[MAXCLIENT];
@@ -26,8 +39,10 @@ bind(serverfd,(struct sockaddr*)&serveraddr,serverlen);
 listen(serverfd,100);
 FD_ZERO(&rds);
 FD_SET(serverfd,&rds);
-maxfd =serverfd +1;
+memset(client,-1,sizeof(client));
+char buf[BUFSIZ];
 while(true){
+printf("one loop begin\n");
 FD_ZERO(&rds);
 FD_SET(serverfd,&rds);
 maxfd =serverfd;
@@ -45,6 +60,19 @@ printf("sock error!\n");
 }
 for(int i =0;i<MAXCLIENT;i++){
 if(client[i]>0 &&FD_ISSET(client[i],&rds)){
+	int n,nread;
+n=0;
+while((nread =recv(client[i],buf+n,BUFSIZ-1,0))>0){
+n+=nread;
+}
+if((nread==-1&& errno !=EAGAIN)
+||n==0){
+perror("read error!\n");
+close(client[i]);
+client[i] =-1;
+}
+if(n>0)
+printf("recv error:%s\n",buf);
 }
 }
  if(FD_ISSET(serverfd,&rds)){
@@ -53,6 +81,9 @@ if(clisock == -1)
 {
 printf("accept error!\n");
 }
+convnonblock(clisock);
+
+printf("client accept:%d\n",clisock);
 for(int i =0;i<MAXCLIENT;i++){
 if(client[i]<0){
 client[i]=clisock;
